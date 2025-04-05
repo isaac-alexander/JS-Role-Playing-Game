ee5e75f13d34abc7c3013d8cc3092ac5c3e7f11f
Isaac Alexander <Isaacalexander81@gmail.com>
2025-04-06 00:41:32 +0100

    script.js

--- script.js
+++ script.js
@@ -107,40 +107,40 @@ function update(location) {
 }
 
 function goTown() {
-  update(locations[0]);
+  update(locations[0]); //first object in the location array.. Town square
 }
 
 function goStore() {
-  update(locations[1]);
+  update(locations[1]); //second object in the location array.. Store
 }
 
 function goCave() {
-  update(locations[2]);
+  update(locations[2]); //third object in the location array.. Cave
 }
 
 function buyHealth() {
-  if (gold >= 10) {
-    gold -= 10;
-    health += 10;
-    goldText.innerText = gold;
-    healthText.innerText = health;
+  if (gold >= 10) { // 50 >= 10 true
+    gold -= 10; //50 - 10 = 40
+    health += 10; // 100 + 10 = 110
+    goldText.innerText = gold; //40
+    healthText.innerText = health; //110
   } else {
-    text.innerText = "You do not have enough gold to buy health.";
+    text.innerText = "You do not have enough gold to buy health."; //"You do not have enough gold to buy health."
   }
 }
 
 function buyWeapon() {
-  if (currentWeapon < weapons.length - 1) {
-    if (gold >= 30) {
-      gold -= 30;
-      currentWeapon++;
-      goldText.innerText = gold;
-      let newWeapon = weapons[currentWeapon].name;
-      text.innerText = "You now have a " + newWeapon + ".";
-      inventory.push(newWeapon);
-      text.innerText += " In your inventory you have: " + inventory;
+  if (currentWeapon < weapons.length - 1) { //0 < 3 = true
+    if (gold >= 30) { //50 >= 30 true
+      gold -= 30; // 50 - 30 = 20
+      currentWeapon++; // 0 + 1= 1
+      goldText.innerText = gold; // 20
+      let newWeapon = weapons[currentWeapon].name; //dagger
+      text.innerText = "You now have a " + newWeapon + "."; //"You now have a dagger."
+      inventory.push(newWeapon); //dagger
+      text.innerText += " In your inventory you have: " + inventory; //" In your inventory you have: dagger "
     } else {
-      text.innerText = "You do not have enough gold to buy a weapon.";
+      text.innerText = "You do not have enough gold to buy a weapon."; //if gold is not > or = 30 "You do not have enough gold to buy a weapon." 
     }
   } else {
     text.innerText = "You already have the most powerful weapon!";
@@ -150,14 +150,14 @@ function buyWeapon() {
 }
 
 function sellWeapon() {
-  if (inventory.length > 1) {
-    gold += 15;
-    goldText.innerText = gold;
-    let currentWeapon = inventory.shift();
-    text.innerText = "You sold a " + currentWeapon + ".";
-    text.innerText += " In your inventory you have: " + inventory;
+  if (inventory.length > 1) { //if inventory contains more than one item. inventory = ["stick","dagger"] 2 > 1 true 
+    gold += 15; // 50 + 15 = 65
+    goldText.innerText = gold; //65
+    let currentWeapon = inventory.shift(); // "stick"
+    text.innerText = "You sold a " + currentWeapon + "."; //"You sold a stick"
+    text.innerText += " In your inventory you have: " + inventory; //" In your inventory you have: dagger."
   } else {
-    text.innerText = "Don't sell your only weapon!";
+    text.innerText = "Don't sell your only weapon!"; //if inventory is <= 1 then. "Don't sell your only weapon!"
   }
 }
 
@@ -177,43 +177,43 @@ function fightDragon() {
 }
 
 function goFight() {
-  update(locations[3]);
-  monsterHealth = monsters[fighting].health;
+  update(locations[3]); // fourth object in the locationn array
+  monsterHealth = monsters[fighting].health; // monsterHealth =monsters[0].health.. monsterHealth = 15
   monsterStats.style.display = "block";
-  monsterName.innerText = monsters[fighting].name;
-  monsterHealthText.innerText = monsterHealth;
+  monsterName.innerText = monsters[fighting].name; //"Slime"
+  monsterHealthText.innerText = monsterHealth; //15
 }
 
 function attack() {
-  text.innerText = "The " + monsters[fighting].name + " attacks.";
-  text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
-  health -= getMonsterAttackValue(monsters[fighting].level);
-  if (isMonsterHit()) {
-    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;    
+  text.innerText = "The " + monsters[fighting].name + " attacks."; //"The slime attacks."
+  text.innerText += " You attack it with your " + weapons[currentWeapon].name + "."; //" You attack it with your dagger."
+  health -= getMonsterAttackValue(monsters[fighting].level); // 100 -= 5 * 2 = 10. 100 -10 =90 . health = 90
+  if (isMonsterHit()) { //If true: Player hits the monster, and the next block executes. If false it moves to the else block
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1; // monsterHealth -= 30 + 0 + 1. monsterHealth -= 31. 15 - 31= -16(defeated)
   } else {
     text.innerText += " You miss.";
   }
-  healthText.innerText = health;
-  monsterHealthText.innerText = monsterHealth;
-  if (health <= 0) {
+  healthText.innerText = health; // 90
+  monsterHealthText.innerText = monsterHealth; // 16
+  if (health <= 0) {  // false
     lose();
-  } else if (monsterHealth <= 0) {
-    if (fighting === 2) {
+  } else if (monsterHealth <= 0) { //true
+    if (fighting === 2) {  // false fighting === 0 slime
       winGame();
     } else {
-      defeatMonster();
+      defeatMonster(); // defeat monster
     }
   }
-  if (Math.random() <= .1 && inventory.length !== 1) {
-    text.innerText += " Your " + inventory.pop() + " breaks.";
-    currentWeapon--;
+  if (Math.random() <= .1 && inventory.length !== 1) {  //true inventory ["stick", "dagger"]
+    text.innerText += " Your " + inventory.pop() + " breaks."; //Your dagger breaks. 
+    currentWeapon--; //"stick"
   }
 }
 
 function getMonsterAttackValue(level) {
   const hit = (level * 5) - (Math.floor(Math.random() * xp));
   console.log(hit);
-  return hit > 0 ? hit : 0;
+  return hit > 0 ? hit : 0; // if hit is greater than 0 return hit else return 0
 }
 
 function isMonsterHit() {
@@ -221,39 +221,39 @@ function isMonsterHit() {
 }
 
 function dodge() {
-  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name;  // "You dodge the attack from the slime"
 }
 
 function defeatMonster() {
-  gold += Math.floor(monsters[fighting].level * 6.7);
-  xp += monsters[fighting].level;
-  goldText.innerText = gold;
-  xpText.innerText = xp;
-  update(locations[4]);
+  gold += Math.floor(monsters[fighting].level * 6.7); // gold += 2*6.7 =13.4. gold += 13 . gold = 63
+  xp += monsters[fighting].level; // xp = 2
+  goldText.innerText = gold; //63
+  xpText.innerText = xp; //2
+  update(locations[4]); //fifth object in the  array (location[4]).
 }
 
 function lose() {
-  update(locations[5]);
+  update(locations[5]); //6th object in the array (loaction[5])
 }
 
 function winGame() {
-  update(locations[6]);
+  update(locations[6]); //7th object in the array (loaction[6])
 }
 
 function restart() {
-  xp = 0;
-  health = 100;
-  gold = 50;
-  currentWeapon = 0;
-  inventory = ["stick"];
-  goldText.innerText = gold;
-  healthText.innerText = health;
-  xpText.innerText = xp;
+  xp = 0; // reset xp to 0
+  health = 100; // reset health to 100
+  gold = 50; // reset gold to 50
+  currentWeapon = 0; //resets to default weapon stick
+  inventory = ["stick"]; // clears out inventory and resets to stick
+  goldText.innerText = gold; //gold
+  healthText.innerText = health; //health
+  xpText.innerText = xp; //xp
   goTown();
 }
 
 function easterEgg() {
-  update(locations[7]);
+  update(locations[7]); //8th object in the  array array (location[7])
 }
 
 function pickTwo() {
@@ -265,9 +265,9 @@ function pickEight() {
 }
 
 function pick(guess) {
-  const numbers = [];
-  while (numbers.length < 10) {
-    numbers.push(Math.floor(Math.random() * 11));
+  const numbers = []; //[] after looping and pushing to the array.numbers = [3, 7, 10, 2, 8, 5, 6, 9, 0, 1];
+  while (numbers.length < 10) { //true the loops keeps being true till numbers array lenght is not less than 10
+    numbers.push(Math.floor(Math.random() * 11)); // example numbers = [3, 7, 10, 2, 8, 5, 6, 9, 0, 1];
   }
   text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
   for (let i = 0; i < 10; i++) {
